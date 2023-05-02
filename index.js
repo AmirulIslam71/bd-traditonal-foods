196936950778a87d791ff720b0e68c0fcc47acf1
amirulislam71 <amirultechon@gmail.com>
2023-05-02 23:34:21 +0600

    chefs info data loads

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 
+const chefsInfo = require("./data.json");
+
+app.use(cors());
+
 app.get("/", (req, res) => {
   res.send("data is running");
+});
+
+app.get("/allData", (req, res) => {
+  res.send(chefsInfo);
 });
 
 app.listen(port, () => {
